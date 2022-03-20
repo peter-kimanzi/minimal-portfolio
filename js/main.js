3e102cdcfbb6cfd6ec10a013b75eca1d5016d69e
peter kimanzi <kimjayden001@gmail.com>
2022-03-20 19:02:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+window.addEventListener('load', AOS.refresh)
+
+
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+ 
+
